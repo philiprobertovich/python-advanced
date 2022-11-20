ea4535afd1a86cb14571199177632c773a989dc7
Phil Kendall <philiprobertovich@gmail.com>
2022-11-20 06:36:12 -0800

    create remove duplicate function.

--- python_advanced.py
+++ python_advanced.py
@@ -1,1 +1,13 @@
 from faker import Faker
+
+fake = Faker()
+color_list = []
+
+for i in range(20):
+  color = fake.color_name()
+  color_list.append(color)
+
+def remove_dups(color_lst):
+  set(color_lst)
+  return color_lst
+
