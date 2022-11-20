a213447678c21c6a512ed7f7c7a91058de8d49f4
Phil Kendall <philiprobertovich@gmail.com>
2022-11-20 08:25:49 -0800

    create pytest.

---
+++ test_remove_dups.py
@@ -0,0 +1,10 @@
+from python_advanced import remove_dups
+
+@pytest.mark.parametrize("color_lst, return_value", [
+  (["green", "green", "green", "blue", "blue", "blue", "red"], {"green","blue","red"})
+  ([], {})
+  ("blue", {"blue"})
+])
+
+def test_remove_dups(color_lst, return_value):
+  assert(remove_dups(color_lst) == return_value)
