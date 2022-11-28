7286689a426e7e5e87d1b31bed484a25713c804e
Phil Kendall <philiprobertovich@gmail.com>
2022-11-28 01:00:53 -0800

    modify remove_dup function so that it does not just change the list to a set, but properly removes duplicates from a list.

--- python_advanced.py
+++ python_advanced.py
@@ -9,8 +9,17 @@ for i in range(20):
   color_list.append(color)
 
 def remove_dups(color_lst):
-  set(color_lst)
-  return color_lst
+  var_storage = None
+  counter = 0
+  for color in color_lst:
+    var_storage = color
+    for other_color in color_lst:
+      if var_storage == other_color and counter > 1:
+        color_lst.remove(other_color)
+      counter += 1  
+
+  # set(color_lst)
+  # return color_lst
 
 color_dict = {color: len(color) for color in color_list}
 
